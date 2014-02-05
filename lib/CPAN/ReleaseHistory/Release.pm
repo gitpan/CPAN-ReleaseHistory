6179e2006784504f55831782216b83af116a5db5
Neil Bowers <neil@bowers.com>
2014-02-05 23:24:35 +0000

    Import of NEILB/CPAN-ReleaseHistory-0.01 from CPAN.
    gitpan-cpan-distribution: CPAN-ReleaseHistory
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NEILB/CPAN-ReleaseHistory-0.01.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

---
+++ lib/CPAN/ReleaseHistory/Release.pm
@@ -0,0 +1,21 @@
+package CPAN::ReleaseHistory::Release;
+{
+  $CPAN::ReleaseHistory::Release::VERSION = '0.01';
+}
+
+use Moo;
+use CPAN::DistnameInfo;
+
+has 'path'      => (is => 'ro');
+has 'timestamp' => (is => 'ro');
+has 'size'      => (is => 'ro');
+has 'distinfo'  => (is => 'lazy');
+
+sub _build_distinfo
+{
+    my $self = shift;
+
+    return CPAN::DistnameInfo->new($self->path);
+}
+
+1;
